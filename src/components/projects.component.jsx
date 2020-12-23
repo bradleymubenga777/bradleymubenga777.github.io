22fd67ccdc9fa3e1d26cf15823e600aff6d2874e
Bradley Mubenga <bradleymubenga777@gmail.com>
2020-12-23 05:11:43 +0200

    Portfolio-Development

--- src/components/projects.component.jsx
+++ src/components/projects.component.jsx
@@ -1,39 +1,41 @@
 import React from 'react'
 import '../main.css'
 import profilePicture from '../img/profilePicture.jpg'
+import portfolioPicture from '../img/portfolioPicture.jpeg'
 
 export default function Projectst() {
     return (
         <div className="row">
             <div className="col-md-12 col-lg-12">
                 <div className="vertColumn container mb-3 pt-3 pb-4">
-                    <h4 className="">Projects</h4>
+                    <h4 className="border-bottom pb-3">Projects</h4>
 
-                    <div className="d-flex flex-row flex-wrap">
-                        <div className="m-2">
-                            <img src={profilePicture} alt="" width="45"/>
+                    <div className="row">
+                        <div className="col-md-12 col-xl-6">
+                            <div class="card mb-3">
+                                <img src={portfolioPicture} class="card-img-top img-fluid" alt="..." />
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
                         </div>
 
-                        <div className="m-2">
-                            <img src={profilePicture} alt="" width="45"/>
-                        </div>
+                        <div className="col-md-12 col-xl-6">
+                            <div class="card mb-3">
+                                <img src={portfolioPicture} class="card-img-top img-fluid" alt="..." />
 
-                        <div className="m-2">
-                            <img src={profilePicture} alt="" width="45"/>
-                        </div>
-
-                        <div className="m-2">
-                            <img src={profilePicture} alt="" width="45"/>
-                        </div>
-
-                        <div className="m-2">
-                            <img src={profilePicture} alt="" width="45"/>
-                        </div>
-
-                        <div className="m-2">
-                            <img src={profilePicture} alt="" width="45"/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    
                 </div>
             </div>
 
--- src/components/techStack.component.jsx
+++ src/components/techStack.component.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import '../main.css'
 import Projects from '../components/projects.component'
 import profilePicture from '../img/profilePicture.jpg'
+import pic1 from '../img/pic (1).png'
+import pic2 from '../img/pic (2).png'
+import pic3 from '../img/pic (3).png'
+import pic4 from '../img/pic (4).png'
+import pic5 from '../img/pic (5).png'
+import pic6 from '../img/pic (6).png'
+import pic7 from '../img/pic (7).png'
 
 
 export default function techStack() {
@@ -14,24 +21,17 @@ export default function techStack() {
                                 <h4>Front-end</h4>
 
                                 <div className="d-flex flex-row flex-wrap">
+
                                     <div className="m-2">
-                                        <img src={profilePicture} alt="" width="45"/>
+                                        <img src={pic1} alt="" width="45"/>
+                                    </div>
+                                    
+                                    <div className="m-2">
+                                        <img src={pic5} alt="" width="45"/>
                                     </div>
                 
                                     <div className="m-2">
-                                        <img src={profilePicture} alt="" width="45"/>
-                                    </div>
-
-                                    <div className="m-2">
-                                        <img src={profilePicture} alt="" width="45"/>
-                                    </div>
-
-                                    <div className="m-2">
-                                        <img src={profilePicture} alt="" width="45"/>
-                                    </div>
-                
-                                    <div className="m-2">
-                                        <img src={profilePicture} alt="" width="45"/>
+                                        <img src={pic6} alt="" width="45"/>
                                     </div>
 
                                 </div>
@@ -40,27 +40,24 @@ export default function techStack() {
 
                     <div className="col-md-12 col-lg-6">
                         <div className="vertColumn container mb-3 pt-3 pb-4">
-                            <h4>Front-end</h4>
+                            <h4>Back-end</h4>
 
                             <div className="d-flex flex-row flex-wrap">
+
                                 <div className="m-2">
-                                    <img src={profilePicture} alt="" width="45"/>
+                                    <img src={pic3} alt="" width="45"/>
                                 </div>
             
                                 <div className="m-2">
-                                    <img src={profilePicture} alt="" width="45"/>
+                                    <img src={pic2} alt="" width="45"/>
                                 </div>
 
                                 <div className="m-2">
-                                    <img src={profilePicture} alt="" width="45"/>
+                                    <img src={pic7} alt="" width="45"/>
                                 </div>
 
                                 <div className="m-2">
-                                    <img src={profilePicture} alt="" width="45"/>
-                                </div>
-            
-                                <div className="m-2">
-                                    <img src={profilePicture} alt="" width="45"/>
+                                    <img src={pic4} alt="" width="45"/>
                                 </div>
 
                             </div>
