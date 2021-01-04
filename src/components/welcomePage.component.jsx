67103c2b7b2a2e06d6049d13c56776bf91bb428d
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-01-04 12:28:40 +0200

    React Update: Welcome Page With Router

--- src/components/welcomePage.component.jsx
+++ src/components/welcomePage.component.jsx
@@ -9,21 +9,21 @@ export default class welcomePage extends Component {
             <div>
                 <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 mt-5">
                     <div className="vertColumn container">
-                        <div className="row">
+                        <div className="row pt-3 pb-3">
         
                             <div className="col-lg-6 d-flex align-items-center justify-content-center">
-                                <div className="pt-3 pb-3 text-center">
+                                <div className="text-center">
                                     <img src={profilePicture} alt="" width="400rem" className="img-fluid roundImg"/>
                                 </div>
                             </div>
         
                             <div className="col-lg-6 d-flex flex-column align-items-center justify-content-center text-center">
-                                <div>
+                                <div className="mt-2">
                                     <h1 className="">Hello World!</h1>
                                 </div>
         
-                                <div className="text-justify container">
-                                    <p>Self-motivated developer, who is willing to learn and create outstanding web applications to solve problems. Self-motivated developer, who is willing to learn and create outstanding web applications to solve problems.</p>
+                                <div className="text-justify mx-3">
+                                    <p className="lead">My name is Bradley Mubenga, I solve problems for businesses and individuals by creating web applications that meet their needs. Whether your goal is to have a beautiful website or have a web application with unique features, I've got you covered! Click one of the links below and let's solve your problem.</p>
                                 </div>
         
                                 <div className="d-flex flex-row flex-wrap align-items-center pb-3 theLinks">
