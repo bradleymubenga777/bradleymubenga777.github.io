a9db2a933c2fe49b833ea0ff2eee20e2acdf991a
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-01-06 07:23:04 +0200

    Update: New Design For SPA

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,18 +1,54 @@
 import React from 'react'
-export default function navbar() {
+import logoImg from '../img/logoImg.png'
+
+
+export default function Navbar() {
     return (
-        <nav class="col-12 navbar d-none navbar-expand-lg vertColumn ">
-                    <ul class="navbar-nav mr-auto">
-                        <div href="" className="mr-1">
-                            <a href="/portfolio">
-                                <button className="btn btn-outline-primary">Portfolio</button>
-                            </a>
-                        </div>
-            
-                        <div href="" className="ml-1">
-                            <button className="btn btn-outline-primary">Blog</button>
-                        </div>
-                    </ul>
-        </nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+        <img alt="" src={logoImg} width="45rem" />
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample07">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+
+        <li class="nav-item active">
+            <a href="/" className="nav-link">Portfolio</a>
+        </li>
+
+        <li class="nav-item active">
+            <a href="/" className="nav-link">Blog</a>
+        </li>
+
+        <li class="nav-item active">
+            <a href="/" className="nav-link">Contact</a>
+        </li>
+        
+        <div className="borderRight ml-3"></div>
+
+        <div className="ml-3 d-flex align-items-center justify-content-center">
+            <a href="/" className="mr-3">
+                <i className="fa fa-facebook"></i>
+            </a>
+
+            <a href="/" className="mr-3">
+                <i className="fa fa-facebook"></i>
+            </a>
+
+            <a href="/" className="">
+                <i className="fa fa-facebook"></i>
+            </a>
+        </div>
+      </ul>
+    </div>
+  </div>
+</nav>
     )
 }
