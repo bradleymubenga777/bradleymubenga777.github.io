8355df84d1b364ad2fbe0a8b144bf3b9c37ac74a
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-02-08 02:37:44 +0200

    Updated Website

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+$(function() {
+  
+    //FILTER BY TAG
+    $('.filter').on( 'click', 'a', function(e) {
+        e.preventDefault();
+      
+        //Get filter data from active link
+        var filterValue = $(this).attr('data-filter');
+      
+        //Show filtered list by matching data-filter with class of item
+        if(filterValue == "all") {
+          $(".list .item").fadeIn('slow');
+        } else {
+          $(".list .item").not("." + filterValue).hide();
+          $("." + filterValue).fadeIn('slow');
+        }
+      
+        //Change active filter tag
+        $('.filter li').removeClass('active');
+        $(this).closest('li').addClass('active');
+      
+        //Remove search field content when choosing a filter
+        $('.no-result').hide();$('#search').val('');
+    });
+    
+    //FILTER BY SEARCH
+    var rows = $('.list li');
+    $('.no-result').hide();
+    
+    $('#search').keyup(function() {
+        $('.filter li').removeClass('active');
+      
+        var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+        reg = RegExp(val, 'i'),
+        text;
+  
+        rows.show().filter(function() {
+          text = $(this).text().replace(/\s+/g, ' ');
+          return !reg.test(text);
+        }).hide();
+  
+        var visible = $('.item:visible').length;
+  
+        if(visible == 0) { 
+          $('.no-result').show();
+        } else { 
+          $('.no-result').hide();
+        }
+  
+    });
+    
+  });
